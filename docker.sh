d72d3c4fb67ab2ab5b1207a08e8dec41521c470f
hve <hve4638@gmail.com>
2024-04-02 19:56:52 +0900

    edit docker.sh

--- docker.sh
+++ docker.sh
@@ -18,7 +18,7 @@ check_install make
 
 rl=`readlink -f $0`
 scriptdir=`dirname $rl`
-dockercmd="docker run -it -v $scriptdir/vol:/vol --rm canreplay"
+dockercmd="docker run -it -v $scriptdir/vol:/vol --net host --rm canreplay"
 
 if [ "$1" == "build" -o "$1" == "b" ]; then 
     cd $scriptdir
@@ -41,22 +41,26 @@ elif [ "$1" == "interactive" -o "$1" == "i" ]; then
     $dockercmd /bin/bash
 elif [ "$1" == "export" -o "$1" == "e" ]; then
     if [ "$2" == "json" ]; then
-        $dockercmd python3 /code/pcapparser/app.py $3
+        $dockercmd python3 /code/pcapparser/app.py ${@:3}
     elif [ "$2" == "csv" ]; then
-        $dockercmd python3 /code/pcapparser/app.py $3
+        $dockercmd python3 /code/pcapparser/app.py ${@:3}
     elif [ "$2" == "replay" ]; then
-        $dockercmd python3 /code/binarization/app.py $3
+        $dockercmd python3 /code/binarization/app.py ${@:3}
     fi
 elif [ "$1" == "replay" -o "$1" == "r" ]; then
-    $dockercmd /code/canreplay $2
+    $dockercmd /code/canreplay ${@:2}
 elif [ "$1" == "analyze" -o "$1" == "a" ]; then
-    $dockercmd python3 /code/analyzer/app.py $2
+    $dockercmd python3 /code/analyzer/app.py ${@:2}
 elif [ "$1" == "test" -o "$1" == "t" ]; then
     $dockercmd /bin/bash -c umask
 else 
     echo -e "Using: docker.sh <option>"
-    echo -e "\tbuild\tbuild docker image"
-    echo -e "\texport\t"
+    echo -e "\tbuild\t"
+    echo -e "\texport\tjson"
+    echo -e "\t      \tcsv"
+    echo -e "\t      \treplay"
+    echo -e "\treplay"
+    echo -e "\tanalyze"
     echo -e "\tinteractive\t"
 fi
 
